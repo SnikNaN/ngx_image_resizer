85b89603c85e353bd5e820dea814f35d02056cf2
SnikNaN <71414779+SnikNaN@users.noreply.github.com>
2023-05-15 15:39:55 +0300

    Update ngx_http_image_resizer_module.c

--- ngx_http_image_resizer_module.c
+++ ngx_http_image_resizer_module.c
@@ -828,6 +828,9 @@ ngx_http_image_resizer_image_resize(
                 break;
             }
 
+			ngx_uint_t width = resize_size->width;
+    		ngx_uint_t height = resize_size->height;
+		
             white_bg = NewPixelWand();
             if (!white_bg) break;
 
